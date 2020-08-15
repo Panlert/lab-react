6e79fd4fd6d9052747cd43d880a45a36563368a9
Panlert <6110110316@psu.ac.th>
2020-08-15 14:31:39 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 import './App.css';
 
 const word = "Hello"
 function App() {
   return (
     <div>
-        <WordCard value="hello"/>
+        <WordCard value={word}/>
     </div>
   );
 }
