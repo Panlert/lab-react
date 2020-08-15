58e93941db8a6ca6a7e6e0a7aa192772bc566810
Panlert <6110110316@psu.ac.th>
2020-08-15 14:01:35 +0700

    part2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import CharacterCard from './CharacterCard';
 import './App.css';
 
+const word = "Hello"
 function App() {
   return (
     <div>
-      <CharacterCard value="h"/>
-      <CharacterCard value="i"/>
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
     </div>
   );
 }
