f443c8df8f55e85daa567acb65d0e99ac13ac352
Panlert <6110110316@psu.ac.th>
2020-08-24 20:20:52 +0700

    create Random word function

---
+++ src/Win.js
@@ -0,0 +1,13 @@
+/*import React, { Component } from 'react';
+
+    class Win extends Component{
+        render(){
+            return(
+                <div>
+                    <h1><dd>You Won.</dd>
+                    </h1>
+                </div>
+            )
+        }
+    }
+    export default Win;*/
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import _, { attempt } from 'lodash';
+import _, { attempt, set } from 'lodash';
 import CharacterCard from './CharacterCard';
+/*import Lose from './Lose';
+import Win from './Win';*/
+
 const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
+    
     return {
         word,
         chars,
@@ -12,31 +16,53 @@ const prepareStateFromWord = (given_word) => {
         completed: false
     }
 }
+
+//const maxAttempt = 3;
+
 export default function WordCard(props){
+
     const [state, setState] = useState(prepareStateFromWord(props.value))
+    
     const activationHandler = c => {
         console.log(`${c} has been activated.`)
+
         let guess = state.guess + c
         setState({...state, guess})
+
         if(guess.length == state.word.length){
             if(guess == state.word){
                 console.log('yeah!')
                 setState({...state, guess: '', completed: true})
+                
             }else{
                 console.log('reset, next attempt')
+                console.log('attempt = %d',state.attempt)
                 setState({...state, guess: '', attempt: state.attempt + 1})
             }
+        
         }
     }
 
-           
-    return (
-        <div>
-           {
-                state.chars.map((c, i) => 
-                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
-                    )
-            }
-        </div>
-    )
+   /* if(state.attempt>maxAttempt){
+        return (
+            <Lose></Lose>
+        )
+    }
+    if(state.completed){
+        return (
+            <Win></Win>
+        )
+    }
+    else{*/
+        return (
+            <div>
+                {
+                    state.chars.map((c, i) => 
+                        <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
+                        )
+                }
+            </div>
+        )
+    //}
+    
 }
