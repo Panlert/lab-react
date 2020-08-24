d054d1fd9ba63e382bb8d082f75103342a45e2e3
Panlert <6110110316@psu.ac.th>
2020-08-24 20:33:38 +0700

    random word and  create 3 component which not a same word

--- src/App.js
+++ src/App.js
@@ -16,21 +16,21 @@ function App() {
   }
 
   var word1 = wordRand();
-  /*var word2 = wordRand();
+  var word2 = wordRand();
   var word3 = wordRand();
   while(word1 == word2||word1 == word3||word2==word3){
       word1 = wordRand();
       word2 = wordRand();
       word3 = wordRand();
-  }*/
+  }
 
   return (
     <div><WordCard value={word1}/></div>
-    /*<div>
+    <div>
         <WordCard value={word1}/>
         <WordCard value={word2}/>
         <WordCard value={word3}/>
-    </div>*/
+    </div>
   );
     
 }
